3f8e76c8fe5ea737db239c1a7224d54fc98f76d0
coledotjs <216449108+coledotjs@users.noreply.github.com>
2025-06-30 13:20:43 +0200

    chore(commands): Add example with `Args` class.

---
+++ src/commands/Greet.ts
@@ -0,0 +1,74 @@
+import {
+	ApplicationCommandOptionType,
+	ApplicationCommandType,
+	BaseMessageOptions,
+	ChatInputCommandInteraction,
+	Message
+} from "discord.js";
+
+import { Args } from "@structures/Args.js";
+import Command, { CommandCategory } from "@structures/Command.js";
+
+const options: BaseMessageOptions = {
+	allowedMentions: { parse: ["users"] }
+};
+
+export default class Greet extends Command {
+	constructor() {
+		super({
+			name: "greet",
+			type: ApplicationCommandType.ChatInput,
+			aliases: ["hi"],
+			category: CommandCategory.General,
+			description: "Greet another person.",
+			options: [
+				{
+					name: "target",
+					description: "The user to greet.",
+					type: ApplicationCommandOptionType.User,
+					required: true
+				}
+			]
+		});
+	}
+
+	override async executeMessage(message: Message<true>, args: Args) {
+		// If there are no available arguments.
+		if (args.finished) {
+			return message.channel.send({
+				content: `${message.author}, you must provide a user to greet.`,
+				...options
+			});
+		}
+
+		// In this case, this will attempt to retrieve a member from the first argument in the parser.
+		const target = await args.getMember();
+
+		if (!target) {
+			return message.channel.send({
+				content: `${message.author}, that is not a valid user.`,
+				...options
+			});
+		}
+
+		return message.channel.send({
+			content: `${target}, you have a greeting from ${message.author}!`,
+			...options
+		});
+	}
+
+	override async executeInteraction(interaction: ChatInputCommandInteraction<"cached">) {
+		const target = interaction.options.getUser("target", true);
+
+		if (!target) {
+			return interaction.reply({
+				content: `That is not a valid user.`
+			});
+		}
+
+		return interaction.reply({
+			content: `${target}, you have a greeting from ${interaction.user}!`,
+			...options
+		});
+	}
+}
